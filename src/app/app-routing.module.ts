1fab3462a38555560d4a1194d33b9c9961e95a8f
Bálint Csongor-Tivadar <balint.csongor@student.ms.sapientia.ro>
2024-03-23 00:59:56 +0200

    created basic navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent,
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent,
   },
 ];
 
