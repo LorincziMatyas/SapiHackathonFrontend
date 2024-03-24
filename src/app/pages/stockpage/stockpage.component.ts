e51fe7b5c96551c515a3a749f30594d995fcc5b8
Bálint Csongor-Tivadar <balint.csongor@student.ms.sapientia.ro>
2024-03-24 11:23:13 +0200

    updated home and stockpage

--- src/app/pages/stockpage/stockpage.component.ts
+++ src/app/pages/stockpage/stockpage.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Company, Stock, StockLogs } from 'src/app/models/stock';
 import { StockService } from 'src/app/services/stock.service';
 import { forkJoin } from 'rxjs';
-
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-stockpage',
   templateUrl: './stockpage.component.html',
@@ -13,23 +14,16 @@ export class StockpageComponent implements OnInit {
   stockLogs!: StockLogs[];
   data: number[][] = [];
   companies: Company[] = [];
+  chartInstances: Chart[] = [];
+  // chartInstances: CanvasJS.Chart[] = [];
 
   myCompany: Company = {} as Company;
   compbudget: number = 1000;
   money: number = 0;
+  stockQuantity: number[] = [];
   myStocks: Stock[] = [];
 
-  chartOptions: any = {
-    scales: {
-      xAxes: [
-        {
-          time: {
-            unit: 'day', // Adjust unit as needed (e.g., 'month', 'year')
-          },
-        },
-      ],
-    },
-  };
+  chartOptions: any[] = [];
 
   constructor(private service: StockService) {}
 
@@ -44,6 +38,7 @@ export class StockpageComponent implements OnInit {
         this.stockLogs = value.stockLogs;
         this.companies = value.companies;
         this.processData();
+        this.stockQuantity = Array(this.stocks.length).fill(0);
       },
       error: (error: any) => {
         console.error('Error fetching data:', error);
@@ -87,6 +82,7 @@ export class StockpageComponent implements OnInit {
 
   processData(): void {
     this.data = [];
+    this.chartOptions = [];
 
     for (const stock of this.stocks) {
       stock.stock_prices = [];
@@ -105,49 +101,65 @@ export class StockpageComponent implements OnInit {
 
       if (stock.stock_prices.length > 0) {
         this.data.push([...stock.stock_prices]);
+        this.chartOptions.push(this.getChartOptions(stock));
       }
     }
-
-    /////////////////////////////////////
   }
 
-  sellStock(stock: Stock): void {
+  getChartOptions(stock: Stock): any {
+    return {
+      theme: 'dark2',
+      axisY: {
+        prefix: '$',
+      },
+      toolTip: {
+        shared: true,
+      },
+      title: {
+        text: `Stock price history - ${stock.company_name}`,
+      },
+      data: [
+        {
+          type: 'line',
+          showInLegend: true,
+          dataPoints: this.getDataPoints(stock),
+        },
+      ],
+    };
+  }
+
+  getDataPoints(stock: Stock): any[] {
+    return stock.stock_prices.map((price, index) => ({
+      x: index,
+      y: price,
+    }));
+  }
+
+  getChartInstance(event: any, index: number): void {
+    this.chartInstances[index] = event.chart;
+  }
+
+  sellStock(stock: Stock, id: number): void {
     const price = stock.stock_price;
     if (this.myStocks.includes(stock)) {
       this.myStocks.splice(this.myStocks.indexOf(stock), 1);
+      this.stockQuantity[id]--;
 
       this.compbudget += price;
       this.money -= price;
       console.log(this.myStocks);
     }
-
-    // const company = this.companies.filter((company) => {
-    //   if (company.id === this.appComponent.loadUserData().company_id) {
-    //     company.budget += price;
-    //   }
-    // });
-
-    // TODO: update company budget
   }
 
-  buyStock(stock: Stock): void {
+  buyStock(stock: Stock, id: number): void {
     if (this.compbudget < stock.stock_price) {
       return;
     }
     const price = stock.stock_price;
     this.compbudget -= price;
+    this.stockQuantity[id]++;
     this.money += price;
     this.myStocks.push(stock);
     console.log(this.myStocks);
-
-    // const company = this.companies.filter((company) => {
-    //   if (company.id === this.appComponent.loadUserData().company_id) {
-    //     if (company.budget >= price) {
-    //       company.budget -= price;
-    //     }
-    //   }
-    // });
-
-    // TODO: update company budget
   }
 }
